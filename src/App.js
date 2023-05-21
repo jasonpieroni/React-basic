e2e0c96fde85abccbc896c322fdef2492678967c
jasonpieroni <jasonpieroni@gmail.com>
2023-05-21 13:00:49 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('')
+  const [message, setMessage] = useState('')
+
+const handleSubmit = () => {
+  if (color) {
+    setMessage(`Thanks for selecting ${color}`)
+  } else {
+    setMessage('Please select a color')
+  }
+}
+
+const handleColorChange = (e) => {
+  setColor(e.target.value)
+}
   return (
     <div className="App">
       <h1>Pick Your Favorite Color</h1>
-      <select name="list" id="list">
-      <option value="blue">blue</option>
-      <option value="red">red</option>
-      <option value="green">green</option>
-      <option value="yellow">yellow</option>
-      <option value="purple">purple</option>
-      <option value="orange">orange</option>
+      <select name="list" id="list" value={color} onChange={handleColorChange}>
+      <option value='blue'>blue</option>
+      <option value='red'>red</option>
+      <option value='green'>green</option>
+      <option value='yellow'>yellow</option>
+      <option value='purple'>purple</option>
+      <option value='orange'>orange</option>
       </select>
+      <button type='submit' onClick={handleSubmit}>Submit</button>
+      <br />
+      <p>{message}</p>
     </div>
   );
 }
